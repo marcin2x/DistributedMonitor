6b78d41bbb824e13bbfe0aa7096fb7d75e9178dc
gremlingg7 <gremlingg7@interia.pl>
2017-05-07 16:20:58 +0200

    Add files via upload

--- src/sensor/sensor.py
+++ src/sensor/sensor.py
@@ -14,9 +14,12 @@ from src.common.TCP_messages import *
 BUFFER_SIZE = 1024
 QUIET_MODE = False
 
+def generate_id():
+    return str(uuid.uuid4())
+
 def prepare_metadata(metadata):
     fields={
-            "CPU Core count": lambda: psutil.NUM_CPUS,
+            "CPU Core count": lambda: psutil.cpu_count(),
             "Total RAM": lambda: psutil.virtual_memory().total
             }
     result = []
@@ -25,11 +28,10 @@ def prepare_metadata(metadata):
             result.append({"key": key, "value": fields[key]()})
     return result
 
-def prepare_register(data, sensor_id = ""):
-    identifier = str(uuid.uuid4()) if sensor_id == "" else sensor_id
-    name = "some PC name"
+def prepare_register(data):
+    name = socket.gethostname()
     measurements = [{"name": x } for x in data["measurements"]]
-    result = SensorRegisterRequest(identifier, name, measurements, prepare_metadata(data["metadata"]))
+    result = SensorRegisterRequest(data["identifier"], name, measurements, prepare_metadata(data["metadata"]))
     return result
 
 def prepare_data(response):
@@ -46,10 +48,10 @@ def prepare_data(response):
             result = SensorDataRequest(response.sensor_id, data)
     return result
 
-def register(config, conn, sensor_id=""):
+def register(config, conn):
     reg_data = prepare_register(config)
     if not QUIET_MODE:
-        print ("Register: ", reg_data)
+        print ("Register: ", reg_data.get_message_body())
     s = socket.socket()
     s.connect(conn)
     s.sendall(serializer.serialize(reg_data))
@@ -67,17 +69,22 @@ def register(config, conn, sensor_id=""):
 def run():
     with open('sensor_config.json') as data_file:
         config = json.load(data_file)
+    
+    if config["identifier"] == "":
+        config["identifier"] = generate_id()
+        with open("sensor_config.json","w") as con_file:
+            json.dump(config, con_file)
+    
 
     conn_tuple = (config["host"], config["port"])
     reg_response = register(config,conn_tuple)
     rtime = time.time()
- 
 
     while True:
         dtime = time.time()
-        if time.time() - rtime > config["register_interval"]:
+        if config["register_interval"]> 0 and time.time() - rtime > config["register_interval"]:
             rtime = time.time()
-            reg_response = register(config, conn_tuple, reg_response.sensor_id)
+            reg_response = register(config, conn_tuple)
             if not QUIET_MODE:
                 print("Register response: ", reg_response.get_message_body())
         data = prepare_data(reg_response)
